eb2a0fb3b1942c357ea7cffd6605001a448c112e
Sean O'Neal <17163675+sean-oneal@users.noreply.github.com>
2017-11-18 17:21:16 -0800

    Fix loop to eval num, update naming conventions and messages

--- main.go
+++ main.go
@@ -5,12 +5,13 @@ import (
 )
 
 func main() {
-	numSlice := []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
-	for i := range numSlice {
-		if i%2 == 0 {
-			fmt.Println("even")
+	numbers := []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+
+	for _, num := range numbers {
+		if num%2 == 0 {
+			fmt.Println(num, " is even")
 		} else {
-			fmt.Println("odd")
+			fmt.Println(num, " is odd")
 		}
 	}
 }
